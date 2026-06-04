ffd660a2c69ac3c61f32071ccf937862b7e44d1a
Mani Lashgari <manilashgari@gmail.com>
2026-06-04 13:31:34 +0330

    Type hinting & Docstrings

--- account.py
+++ account.py
@@ -1,6 +1,13 @@
+"""
+account.py
+"""
 from datetime import date
 
+
 class Account:
+    """
+    Represents a bank account.
+    """
     next_id = "1"
 
     def __init__(self, owner: str, balance="0", is_active=True):
@@ -11,14 +18,20 @@ class Account:
         self.is_active = is_active
         self.owner = owner
 
-    def deposit(self, amount: str):
+    def deposit(self, amount: str) -> str:
+        """
+        Deposit money from account.
+        """
         if self.is_active:
             self.balance = int(self.balance) + int(amount)
             return f"Transaction is done successfully.\nCurrent balance: {self.balance}"
         else:
             return "Your account is inactive!"
 
-    def withdraw(self, amount: str):
+    def withdraw(self, amount: str) -> str:
+        """
+        Withdraw money from account.
+        """
         if self.is_active:
             if float(amount) <= float(self.balance):
                 self.balance = int(self.balance) - int(amount)
@@ -27,19 +40,28 @@ class Account:
                 return "ERROR! Not enough balance"
         else:
             return "Your account is inactive!"
-    
-    def get_balance(self):
+
+    def get_balance(self) -> str:
+        """
+        Displaying the balance of accounts.
+        """
         return f"Current balance: {self.balance}"
-    
+
     def get_info(self):
+        """
+        Displaying the information of account.
+        """
         if self.is_active:
             active = "ACTIVE"
         else:
             active = "INACTIVE"
-        return f"Account's information\nAccount owner: {self.owner}\nAccount ID: {self.account_id}\n\
-Current balance: {self.balance}\nDate: {self.date}\nThe account is {active}"
-    
-    def activation(self, active_mode: bool):
+        return f"Account's information\nAccount owner: {self.owner}\nAccount ID: {self.account_id}\
+            \nCurrent balance: {self.balance}\nDate: {self.date}\nThe account is {active}"
+
+    def activation(self, active_mode: bool) -> str:
+        """
+        Displaying the activation of account.
+        """
         self.is_active = active_mode
         if active_mode:
             active = "activated"
--- customer.py
+++ customer.py
@@ -1,17 +1,33 @@
+"""
+Customer.py
+"""
 from account import Account
 
 class Customer:
-    def __init__(self, name: str, customer_id: str, national_id: str, accounts_list: list = []):
+    """
+    Represents a bank customer.
+    """
+    def __init__(self, name: str, customer_id: str, national_id: str, accounts_list: list):
         self.name = name
         self.customer_id = customer_id
         self.national_id = national_id
+        if accounts_list is None:
+            accounts_list = []
         self.accounts_list = accounts_list
 
-    def add_account(self):
+    def add_account(self) -> None:
+        """
+        Creat an account.
+        """
         self.accounts_list.append(Account(self.name).get_info())
 
-    def get_accounts_list(self):
+    def get_accounts_list(self) -> list:
+        """
+        Displaying the list of accounts.
+        """
         return self.accounts_list
-    
-    def get_total_balance(self):
-        ...
+
+    def get_total_balance(self) -> str:
+        """
+        Displaying the balance of accounts.
+        """
