f4cfe3a979ee439c12cfa95a2be3437e13b82d70
DaniyalIranMehr <daniyaliranmehr@gmail.com>
2026-06-25 17:29:16 +0330

    Complete Branch class

--- branch.py
+++ branch.py
@@ -5,23 +5,33 @@ class Branch:
     """
     Reprasent a bank branch.
     """
-    def __init__(self, branch_id: str, branch_address: str,\
-                  customers_list: list, employees_list=None):
-        self.branch_id = branch_id
+    next_id = "1"
+
+    def __init__(self, branch_address: str, employees_list=None):
+        self.branch_id = Branch.next_id
+        Branch.next_id = int(Branch.next_id) + 1
         self.branch_address = branch_address
 
-        if customers_list is None:
-            self.customers_list = []
-        else:
-            self.customers_list = customers_list
-
         if employees_list is None:
-            self.employees_list = []
+            employees_list = []
         else:
             self.employees_list = employees_list
 
-    def assign_employee(self, employee_name : str) -> None:
+    def add_employee(self, name: str) -> None:
         """
-        Assign an employee to the branch.
+        Add an employee to the employees list.
         """
-        self.employees_list.append(employee_name)
+        self.employees_list.append(name)
+
+    def get_employees_list(self) -> list:
+        """
+        Displaying the list of employees.
+        """
+        return self.employees_list
+
+    def get_branch_info(self) -> str:
+        """
+        Displaying the information of branch.
+        """
+        return f"Branch's information\nBranch ID: {self.branch_id}\
+            \nBranch adress: {self.branch_address}"
